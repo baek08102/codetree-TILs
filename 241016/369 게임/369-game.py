647e21c1198fecc653a5e258dc226f607cdcd534
baek08102 <168729060+baek08102@users.noreply.github.com>
2024-10-16 14:41:21 +0900

    Title: 369 게임, Time: 112ms, Memory: 25MB, Status: Passed - CodeTree

--- 241016/369 게임/369-game.py
+++ 241016/369 게임/369-game.py
@@ -1,8 +1,17 @@
 n=int(input())
 
 for n in range(1,n+1):
-    
-    if (n%3)==0 or ((n%10)!=0 and ((n%10)%3)==0):
-        print(0,end=" ")
-    else:
-        print(n,end=" ")
+
+    a=(n//10) #a는 n의 10의자리 수
+    b=(n%10) #b는 n의 1의자리 수
+
+    if n<=10 or (n%10)==0:
+        if (n%3)==0:
+            print(0,end=" ")
+        else:
+            print(n,end=" ")
+    else:        
+        if ((a%3))==0 or (b%3)==0 or (n%3)==0:
+            print(0,end=" ")
+        else:
+            print(n,end=" ")
