c7f2db7f4cc2b7afff649adae558fc73e426e3a9
baek08102 <168729060+baek08102@users.noreply.github.com>
2026-09-21 16:23:18 +0900

    [Passed] 입력받아 연산하기 (68ms, 6MB)

---
+++ trail1/입력받아 연산하기/take-input-and-operate.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write($"{a+87}\n{b%10}");
+    }
+}
